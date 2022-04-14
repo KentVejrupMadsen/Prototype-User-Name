a42be3dc904024c2c696956d6659f7baecf40028
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-04-14 15:11:57 +0200

    c

--- code/database/migrations/2022_04_13_142523_person_name_migration.php
+++ code/database/migrations/2022_04_13_142523_person_name_migration.php
@@ -1,70 +1,72 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+
+    
+    return new class extends Migration
     {
-        //
-        Schema::create( 'first_name', 
-            function ( Blueprint $table ) 
-            {
-                $table->id();
-                $table->string( 'content' )->unique();
-            }
-        );
+        
+        public function up()
+        {
+            //
+            Schema::create( 'first_name', 
+                function ( Blueprint $table ) 
+                {
+                    $table->id();
+                    $table->string( 'content' )
+                          ->unique();
+                }
+            );
 
 
-        Schema::create( 'last_name', 
-            function ( Blueprint $table ) 
-            {
-                $table->id();
-                $table->string('content')->unique();
-            }
-        );
+            Schema::create( 'last_name', 
+                function ( Blueprint $table ) 
+                {
+                    $table->id();
+                    $table->string('content')
+                          ->unique();
+                }
+            );
 
 
-        Schema::create( 'person_name', 
-            function ( Blueprint $table ) 
-            {
-                $table->id();
+            Schema::create( 'person_name', 
+                function ( Blueprint $table ) 
+                {
+                    $table->id();
 
-                $table->bigInteger('first_name_id')->unsigned()->index();
+                    $table->bigInteger( 'first_name_id' )
+                          ->unsigned()
+                          ->index();
 
-                $table->json( 'middle_name' );
-                
-                $table->bigInteger('last_name_id')->unsigned()->index();
-                
-                
-                $table->foreign( 'first_name_id' )
-                      ->references( 'id' )
-                      ->on( 'first_name' );
-                
-                $table->foreign( 'last_name_id' )
-                      ->references( 'id' )
-                      ->on( 'last_name' );
-            }
-        );
-    }
+                    $table->json( 'middle_name' );
+                    
+                    $table->bigInteger( 'last_name_id' )
+                          ->unsigned()
+                          ->index();
+                    
+                    
+                    $table->foreign( 'first_name_id' )
+                        ->references( 'id' )
+                        ->on( 'first_name' );
+                    
+                    $table->foreign( 'last_name_id' )
+                        ->references( 'id' )
+                        ->on( 'last_name' );
+                }
+            );
+        }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-        Schema::dropIfExists('person_name');
-        Schema::dropIfExists('last_name');
-        Schema::dropIfExists('first_name');
-    }
-};
+        
+        public function down()
+        {
+            //
+            Schema::dropIfExists( 'person_name' );
+            Schema::dropIfExists( 'last_name' );
+            Schema::dropIfExists( 'first_name' );
+        }
+    };
+
+?>
